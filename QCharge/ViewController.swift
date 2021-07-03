86011855fc1bda1038ddc36ab88ab6eb1392ce23
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-07-03 11:44:14 +0700

    Initial Commit

---
+++ QCharge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QCharge
+//
+//  Created by Prapawit Patthasirivichot on 3/7/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
